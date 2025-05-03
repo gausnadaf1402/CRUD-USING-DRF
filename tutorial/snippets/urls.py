ea164be4ae4bc2e628c3c87faae8429ade59abd5
gaus <nadafgaus1402@gmail.com>
2025-05-03 14:52:03 +0530

    Initial Commit

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('snippets/',views.SnippetList.as_view(),name='snippet_list'),
+    path('snippets/<int:pk>/',views.SnippetDetail.as_view(),name='snippet_detail'),
+]
